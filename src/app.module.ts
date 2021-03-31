72d9b5190b0eaf04393a54748366c51b96e56512
Husni <husnijabir@gmail.com>
2021-03-31 11:43:53 +0530

    Implemented the Caching for GET end points

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, CacheModule, CacheInterceptor } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { User, UserSchema} from './schema/user.schema';
 import { UserImage, UserImageSchema } from './schema/userimages.schema';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    CacheModule.register(),
     MongooseModule.forRoot(process.env.DATABASE_CONN),
     MongooseModule.forFeature([
       {name : User.name, schema: UserSchema},
@@ -16,6 +18,10 @@ import { UserImage, UserImageSchema } from './schema/userimages.schema';
     ])
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,
+  {
+    provide: APP_INTERCEPTOR,
+    useClass: CacheInterceptor
+  }],
 })
 export class AppModule {}
